40aa72158d1512906229b74a25469827d18c40e8
Robbie <robbie.huffman@nundrum.net>
2023-09-18 16:56:56 -0400

    Cleaned up old comments

--- bb/dripnet.clj
+++ bb/dripnet.clj
@@ -45,35 +45,10 @@
 (def lmax (Math/log maxport))
 (def scaling (/ (cols) lmax))
 
-; lsof ----------------------------------------------------------------------------
-;(defn match-n [l] (filter #(= \n (first %)) l))
-;(defn split-n [s] 
-;  (let [clean             (apply str (rest s ))
-;        [left right]      (clojure.string/split clean #"->")
-;        [srcip srcport]   (clojure.string/split left #":")
-;        [destip destport] (clojure.string/split right #":")
-;        ]
-;    {:srcip srcip :srcport srcport :destip destip :destport destport}))
-;
-;(defn lsof-tcp []
-;  (->> (clojure.java.shell/sh "lsof" "-i" "-sTCP:ESTABLISHED" "-n" "-P" "-F" "n" )
-;    :out
-;    clojure.string/split-lines
-;    match-n
-;    (map split-n)))
-;
-;(defn lsof-diff-new [old new]
-;  (nth (clojure.data/diff (set old) (set new)) 1))
-;
-;(def lastlsof (atom (lsof-tcp)))
 ; tcpdump -------------------------------------------------------------------------
-
 (def tcpdump-matches (ref []))
-;(def tcpdump-matches (ref []))
 
 ;16:31:18.577179 enp4s0 Out IP 192.168.2.101.58886 > 192.168.2.112.443: tcp 0
-;(tcpdump-parse-line "16:31:18.577179 enp4s0 Out IP 192.168.2.101.58886 > 192.168.2.112.443: tcp 0")
-;(count "16:31:18.577179 enp4s0 Out IP 192.168.2.101.58886 > 192.168.2.112.443: tcp 0")
 (defn tcpdump-parse-line [s]
   (let [[ts iface dir _ src _ dest proto _] (split s #" ")
         [_ _ _ _ destport]                  (split dest #"\.") 
@@ -96,32 +71,13 @@
                   (recur)
                   ))))))
 
-(comment
-  (identity @tcpdump-matches)
-  (count (identity @tcpdump-matches))
-  (realized? tcpdump-future)
-  (identity tcpdump-future)
-  (identity @tcpdump-future)
-  (p/destroy-tree tcpdump-stream)
-  (dosync(ref-set  tcpdump-matches []))
-  (reset! tcpdump-matches [])
-  )
-
+(comment (identity @tcpdump-matches)
+         (realized? tcpdump-future)
+         (p/destroy-tree tcpdump-stream))
 
 ; output --------------------------------------------------------------------------
-;(def bucket-size (int (Math/ceil (/ maxport (cols)))))
-
-;(defn bucket [i] (quot i bucket-size))
-;(defn bucket [i] (int (Math/ceil (/ i (/ maxport bucket-size)))))
-;(def scaling (/ 229 lmax))
 (defn bucket [i] (int (Math/ceil (* scaling (Math/log i)))))
-;(bucket maxport) (Math/log 10)
 
-(mapv hash-map (range 1 1024)(mapv bucket (range 1 1024)) )
-(mapv hash-map (range 22000 33000 1000) (mapv bucket (range 22000 33000 1000)) )
-;(frequencies [80 80 80 80 443 443 1024 384])
-;{80 4, 443 2, 1024 1, 384 1}
-;(add-drops [80 80 80 80 443 443 1024 384 2999])
 (defn ports-to-drops [portlist]
   (let [f (frequencies portlist)]
     (map (fn [[x c]]
@@ -133,12 +89,7 @@
             (for [drop drops]
               (let [y (:y drop)]
                 (if (< y (dec (lines)))
-                  (assoc drop :y (inc y))
-                  ))))))
-
-;(ports-to-drops @tcpdump-matches)
-;(identity @dropmatrix)
-;(add-drops [80 443 80 1024])
+                  (assoc drop :y (inc y))))))))
 
 (defn move-and-add-drops! []
   (dosync 
@@ -160,57 +111,30 @@
     (print resetcolor)
     ))
 
-;[col [[row sym] [row sym]]]
-;[col [[row sym] [row sym]]]
-;
-
-
-
 (defn print-drops []
   (doseq [{:keys [x y c p]} @dropmatrix]
-    (println (str (pos x y) c))
-    ))
-
-
-;(identity @dropmatrix)
-;(swap! dropmatrix move-drops)
-;(def a (lsof-tcp))
-;(def b (lsof-tcp))
-;(map :destport (lsof-diff-new a b))
+    (println (str (pos x y) c))))
 
 (defn cycle []
-    ;(println (mapv :destport diff))
     (move-and-add-drops!)
     (print cls)
     (print mtl)
     ;(print-background)
-    (print-drops)
-    ;(println @dropmatrix)
-    )
+    (print-drops))
 
 (defn dripnet []
-  ;(swap! dropmatrix conj {:x 12 :y 3 :c "o"})
-  ;(swap! dropmatrix conj {:x 10 :y 13 :c "O"})
   (print cursor-off)
   (print altmode-on)
-  ;(clojure.java.shell/sh "tput" "smcup")
-  ;(print cls)
   (dotimes [i 600]
     (print mtl)
     (cycle)
-    (Thread/sleep 500)
+    (Thread/sleep 250)
     )
-  ;(clojure.java.shell/sh "tput" "rmcup")
   (print altmode-off)
-  (print cursor-on)
-  )
-#_(println "ho")
+  (print cursor-on))
+
 (.addShutdownHook (Runtime/getRuntime) (Thread. (fn [] (println (str altmode-off cursor-on)))))
 
 (println (cols) (lines))
 (dripnet)
-;(doseq [line (line-seq (clojure.java.io/reader *in*))]
-;  (zingprint line)
-;  (println)
-;  )
 
